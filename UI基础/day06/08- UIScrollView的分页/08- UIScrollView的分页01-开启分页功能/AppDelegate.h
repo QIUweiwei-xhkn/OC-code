de8b5a142bb038f9ee496856c6b5a90012ec887e
weiwei <115163734+QIUweiwei-xhkn@users.noreply.github.com>
2024-02-13 19:15:22 +0800

    commit

---
+++ UI基础/day06/08- UIScrollView的分页/08- UIScrollView的分页01-开启分页功能/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  08- UIScrollView的分页01-开启分页功能
+//
+//  Created by wei wei on 2024/2/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
